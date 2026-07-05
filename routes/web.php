0399549895695ea0010d9f8d2d40c77014379fce
tikt4096 <kl.rs9960@gmail.com>
2026-07-06 01:56:34 +0900

    ルート追加、リクエスト追加

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
 use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'index')->name('home');
 Route::get('/songs', [SongController::class, 'index']);
-Route::get('/songs/{song}', [SongController::class, 'show']);
+Route::get('/songs/{song}', [SongController::class, 'show'])->name('songs.show');
 
 Route::get('/characters', [CharacterController::class, 'index']);
 Route::get('/characters/{character}', [CharacterController::class, 'show']);
+
+Route::post('/comment/post', [CommentController::class, 'post']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
